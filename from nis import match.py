2746c68cbb833c94358f2479d846b7b44b89cc30
ErtyPerty <115721409+ErtyPerty@users.noreply.github.com>
2023-01-17 10:16:00 +0000

    Update from nis import match.py

--- from nis import match.py
+++ from nis import match.py
@@ -1,44 +1,31 @@
-from nis import match
 import os
 import sys
 import random
 from time import sleep
 
-grid = [['.' for x in range(4)] for y in range(4)]
-player_icon = "P"
+board = [
+    [".", ".", ".", ".", "."],
+    [".", ".", ".", ".", "."],
+    [".", ".", ".", ".", "."],
+    [".", ".", ".", ".", "."],
+    [".", ".", ".", ".", "."]
+]
 
-def move_player(x, y):
-    # Check if the new position is within the grid boundaries
-    if x < 0 or x > 3 or y < 0 or y > 3:
-        print("Invalid")
-    else:
-        # Move the player to the new position
-        grid[x][y] = player_icon
-        print_grid()
-
-def print_grid():
-    for row in grid:
-        print(" ".join(row))
-
-# Initial position of the player
-player_x = 0
-player_y = 0
-grid[player_x][player_y] = player_icon
-
-print_grid()
+player_x, player_y = 2, 2
+board[player_x][player_y] = "@"
 
 while True:
-    direction = input("Move around the map with WASD: ")
-    match direction:
-        case 'w':
-            grid[player_x][player_y] = '.'
-            move_player(player_x - 1, player_y)
-        case 'a':
-            grid[player_x][player_y] = '.'
-            move_player(player_x, player_y - 1)     
-        case 's':
-            grid[player_x][player_y] = '.'
-            move_player(player_x + 1, player_y)
-        case 'd':
-            grid[player_x][player_y] = '.'
-            move_player(player_x, player_y + 1)
+    for row in board:
+        print(" ".join(row))
+    print("Enter a direction to move (WASD):")
+    direction = input()
+    switch = {
+        "w": lambda: (player_x > 0) and (player_x-1, player_y),
+        "a": lambda: (player_y > 0) and (player_x, player_y-1),
+        "s": lambda: (player_x < len(board)-1) and (player_x+1, player_y),
+        "d": lambda: (player_y < len(board[0])-1) and (player_x, player_y+1)
+    }
+    new_x, new_y = switch.get(direction, lambda: (player_x, player_y))()
+    board[player_x][player_y] = "."
+    player_x, player_y = new_x, new_y
+    board[player_x][player_y] = "@"
