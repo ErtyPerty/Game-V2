a05708113cf1b182c9d9db9d58041275372cc02f
ErtyPerty <115721409+ErtyPerty@users.noreply.github.com>
2023-01-17 10:38:23 +0000

    Update from nis import match.py

--- from nis import match.py
+++ from nis import match.py
@@ -1,7 +1,5 @@
 import os
-import sys
-import random
-from time import sleep
+
 
 board = [
     [".", ".", ".", ".", "."],
@@ -15,17 +13,31 @@ player_x, player_y = 2, 2
 board[player_x][player_y] = "@"
 
 while True:
+    os.system('cls')
     for row in board:
         print(" ".join(row))
     print("Enter a direction to move (WASD):")
     direction = input()
-    switch = {
-        "w": lambda: (player_x > 0) and (player_x-1, player_y),
-        "a": lambda: (player_y > 0) and (player_x, player_y-1),
-        "s": lambda: (player_x < len(board)-1) and (player_x+1, player_y),
-        "d": lambda: (player_y < len(board[0])-1) and (player_x, player_y+1)
-    }
-    new_x, new_y = switch.get(direction, lambda: (player_x, player_y))()
-    board[player_x][player_y] = "."
-    player_x, player_y = new_x, new_y
-    board[player_x][player_y] = "@"
+    match direction:
+        case 'w':
+            if player_x > 0:
+                board[player_x][player_y] = "."
+                player_x -= 1
+                board[player_x][player_y] = "@"
+        case "a":
+            if player_y > 0:
+                board[player_x][player_y] = "."
+                player_y -= 1
+                board[player_x][player_y] = "@"
+        case "s":
+            if player_x < len(board) - 1:
+                board[player_x][player_y] = "."
+                player_x += 1
+                board[player_x][player_y] = "@"
+        case "d":
+            if player_y < len(board[0]) - 1:
+                board[player_x][player_y] = "."
+                player_y += 1
+                board[player_x][player_y] = "@"
+        case default:
+            print("WASD!")
