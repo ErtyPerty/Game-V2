7c2d396ed4302224f93540dab9183bedb7c392c0
ErtyPerty <115721409+ErtyPerty@users.noreply.github.com>
2023-01-17 15:13:45 +0000

    a!

--- from nis import match.py
+++ from nis import match.py
@@ -5,31 +5,34 @@ import time
 from time import sleep
 
 board = [
-    [".", ".", ".", ".", "."],
-    ["🖑", ".", ".", ".", "."],
-    [".", ".", ".", ".", "."],
-    [".", ".", ".", ".", "."],
-    [".", ".", ".", ".", "."]
+    [".", ".", ".", ".", ".",".", ".", ".", ".", "."],
+    ["🖑", ".", ".", ".", ".",".", ".", ".", ".", "."],
+    [".", ".", ".", ".", "E",".", ".", ".", ".", "."],
+    [".", ".", ".", ".", ".",".", ".", ".", ".", "."],
+    [".", "S", ".", ".", ".",".", ".", ".", ".", "."]
 ]
 
-player_x, player_y = 2, 2
+player_x, player_y = 1, 1
 
 npc_x = 3
 npc_y = 3
 
-with open("player_enemy.txt") as file:
-    player_hp, player_attack, enemy_hp, enemy_attack = [int(x) for x in file.read().split()]
 
-def interact_With_Enemy():
+def interact_With_Enemy(player_hp, player_attack, enemy_hp, enemy_attack):
     while player_hp > 0 and enemy_hp > 0:
-        print("Player HP:", player_hp)
-        print("Enemy HP:", enemy_hp)
-        print("Press the 'a' key as fast as you can for 3 seconds to attack the enemy!")
+        print("Hey! I'm evil, so I'm gonna pwn you!")
         start_time = time.time()
         presses = 0
         while time.time() - start_time < 3:
-            if input() == "a":
-                presses += 1
+            os.system('cls')
+            print("Player HP:", player_hp)
+            print("Enemy HP:", enemy_hp)
+            print("Press the buttons that appear on screen to do damage")
+            random_button = random.choice(['f', 'g', 'j', 'k'])
+            print(random_button)
+            player_input = input()
+            if player_input in ['f', 'g', 'j', 'k'] and player_input == random_button:
+                presses += 2
         attack_power = player_attack + (presses * 0.1)
         enemy_hp -= attack_power
         if enemy_hp <= 0:
@@ -38,20 +41,27 @@ def interact_With_Enemy():
         else:
             player_hp -= enemy_attack
             print("Enemy attacked you for", enemy_attack, "damage!")
-
     if player_hp <= 0:
         print("You were defeated.")
+    return player_hp, enemy_hp
 
-# write player and enemy values to text file
+
+
+with open("player_enemy.txt") as file:
+    player_hp, player_attack, enemy_hp, enemy_attack = [int(x) for x in file.read().split()]
+
+
+
 with open("player_enemy.txt", "w") as file:
-    file.write(f"{player_hp} {player_attack} {enemy_hp} {enemy_attack}")
+    file.write(f"{player_hp} {player_attack} {100} {enemy_attack}")
+
+
 
 def interact_With_Tutorial():
-   #NPC Code
     user_Input = input("Hey! Do you want to hear my story? It's very interesting, and you might get something out of it.\n")
 
     if user_Input.lower() == 'yes':
-        words = ""
+        words = "Cant be asked to write this yet, come back later when I have finished writing"
         for char in words:
             sleep(0.05)
             sys.stdout.write(char)
@@ -70,7 +80,10 @@ while True:
             if i == player_x and j == player_y:
                 if board[i][j] == "🖑":
                     new_board[i].append("@")
-                    interact_with_tutorial()
+                    interact_With_Tutorial()
+                elif board[i][j] == "E":
+                    new_board[i].append("@")
+                    player_hp, enemy_hp = interact_With_Enemy(player_hp, player_attack, enemy_hp, enemy_attack)
                 else:
                     new_board[i].append("@")
             else:
