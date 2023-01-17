8edfe064fb0424636e398d73fb6118cf85f5ec77
ErtyPerty <115721409+ErtyPerty@users.noreply.github.com>
2023-01-17 20:12:19 +0000

    a!

--- main.py
+++ main.py
@@ -7,11 +7,11 @@ from time import sleep
 #VARIABLES
 
 board = [
-    ["B", ".", ".", "H", ".",".", ".", ".", ".", "."],
+    ["B", ".", ".", "H", ".",".", ".", "R", ".", "."],
     ["🖑", ".", ".", ".", ".",".", ".", ".", ".", "."],
     [".", ".", ".", ".", "F",".", ".", ".", ".", "."],
     [".", ".", ".", ".", ".",".", ".", ".", ".", "."],
-    [".", "S", ".", ".", ".",".", ".", ".", ".", "E"]
+    [".", "S", ".", ".", ".",".", "D", ".", ".", "E"]
 ]
 
 player_fatigue = 0
@@ -42,6 +42,15 @@ npc_y = 3
 player_inventory = []
 
 #FUNCTIONS
+def pull_out_sword():
+        skill_check = random.randint(1, 20)
+        if skill_check >= 15:
+            print("You successfully pulled the sword out of the stone!")
+            player_inventory.append("Not_a_zelda_ripoff")
+            board[player_x][player_y] = "."
+        else:
+            print("You failed to pull the sword out of the stone. Keep trying.")
+
 def bar():
     global player_gold
     global player_fatigue
@@ -62,6 +71,15 @@ def bar():
         with open("player_gold.txt", "w") as file:
             file.write(str(player_gold))
 
+        drink = input("Would you like to get a drink? On the house")
+        if drink == "yes":
+            print("Here you go!")
+            player_inventory.append("Drink")
+        elif drink == "no":
+            print("Alright buddy, suit yourself!")
+        else:
+            print("Is that a yes or a no?")
+
 def rest_At_Home():
     global player_fatigue
     if player_fatigue > 80:
@@ -70,7 +88,6 @@ def rest_At_Home():
         print("You feel refreshed and your fatigue is now at 0.")
     else:
         print("The entity living under the bed demands a sacrafice, it is unsafe to sleep for now.")
-
 
 def shop():
     global player_gold
@@ -117,6 +134,22 @@ def interact_With_Tutorial():
     else:
         print('Type yes or no')
 
+def interact_With_Dwarf():
+    user_Input = input("Aye laddy! It be me! Yer old pal Groolgruth! Say you look flimsy! Tell you what! I'm thirsty, bring me a drink from the bar and I'll forge you up some armour! Whatda you say?\n")
+
+    if user_Input.lower() == 'yes':
+        if "Drink" in player_inventory:
+            words = "Aye thank ye laddy! Let's forge up some of that armour now!"
+            for char in words:
+                sleep(0.05)
+                sys.stdout.write(char)
+            print("You got dwarf armour!")
+            player_inventory.append("dwarf_armour")
+    elif user_Input.lower() == 'no':
+        print("Aye, it be ok. Come back later if you change your mind")
+    else:
+        print('That be a yes or a no?')
+
 def interact_With_Fountain():
     user_Input = input("It's the town fountain, legend says if you put in 1 gold, you get good luck for 5 years. Do you want to put in 1 gold?\n")
 
@@ -150,25 +183,47 @@ def interact_With_Enemy(player_hp, player_attack, enemy_hp, enemy_attack):
             print(random_button)
             player_input = input()
             if player_input in ['f', 'g', 'j', 'k'] and player_input == random_button:
-                if "quality_sword" in player_inventory:
+                if "quality_sword" and "Not_a_zelda_ripoff" in player_inventory:
+                    presses += 12
+                elif "quality_sword" in player_inventory:
                     presses += 4
+                elif "Not_a_zelda_ripoff" in player_inventory:
+                    presses += 10
                 else: 
                     presses += 2
         attack_power = player_attack + (presses * 0.1)
         enemy_hp -= attack_power
         if enemy_hp <= 0:
+            board[enemy_x][enemy_y] = "."
             print("You defeated the enemy!")
             player_gold + 100
             break
         else:
-            if "quality_armour" in player_inventory:
+            if "quality_armour" and "dwarf_armour" in player_inventory:
+                player_hp -= enemy_attack-25
+            elif "quality_armour" in player_inventory:
                 player_hp -= enemy_attack-10
+            elif "dwarf_armour" in player_inventory:
+                player_hp -= enemy_attack-20
             else:
                 player_hp -= enemy_attack
             print("Enemy attacked you for", enemy_attack, "damage!")
     if player_hp <= 0:
-        print("You were defeated.")
+        print("You were defeated by the dragon.")
+        print("Come back later when you are stronger...")
     return player_hp, enemy_hp
+
+symbol_map = {
+    "🖑": interact_With_Tutorial,
+    "E": interact_With_Enemy,
+    "S": shop,
+    "F": interact_With_Fountain,
+    "B": bar,
+    "H": rest_At_Home,
+    "R": pull_out_sword,
+    "D": interact_With_Dwarf
+}
+
 
 #MAIN LOOP
 
@@ -180,26 +235,8 @@ while True:
         new_board.append([])
         for j in range(len(board[0])):
             if i == player_x and j == player_y:
-                if board[i][j] == "🖑":
-                    new_board[i].append("@")
-                    interact_With_Tutorial()
-                elif board[i][j] == "E":
-                    new_board[i].append("@")
-                    player_hp, enemy_hp = interact_With_Enemy(player_hp, player_attack, enemy_hp, enemy_attack)
-                elif board[i][j] == "S":
-                    new_board[i].append("@")
-                    shop()
-                elif board[i][j] == "F":
-                    new_board[i].append("@")
-                    interact_With_Fountain()
-                elif board[i][j] == "B":
-                    new_board[i].append("@")
-                    bar()
-                elif board[i][j] == "H":
-                    new_board[i].append("@")
-                    rest_At_Home()
-                else:
-                    new_board[i].append("@")
+                new_board[i].append("@")
+                symbol_map.get(board[i][j], lambda: None)()
             else:
                 new_board[i].append(board[i][j])
     for row in new_board:
@@ -222,4 +259,3 @@ while True:
             player_y += 1
     else:
         print("WASD!")
-        
